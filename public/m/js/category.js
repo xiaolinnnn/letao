55c81c92d9930a66f4be4fbbc62db528b37f2eed
xiaolin <939158029@qq.com>
2018-06-10 10:22:55 +0800

    2018-06-10

---
+++ public/m/js/category.js
@@ -0,0 +1,73 @@
+$(function () {
+	var letao = new Letao();
+	// 通过乐淘对象初始化区域滚动
+	letao.initScroll();
+	letao.getCategoryLeft();
+	letao.getCategoryRight();
+});
+
+//Letao的构造函数
+var Letao = function () {
+
+}
+
+Letao.prototype = {
+	//初始化区域滚动
+	initScroll: function () {
+		//初始化区域滚动
+		var options = {
+			scrollY: true, //是否竖向滚动
+			scrollX: false, //是否横向滚动
+			startX: 0, //初始化时滚动至x
+			startY: 0, //初始化时滚动至y
+			indicators: false, //是否显示滚动条
+			deceleration: 0.0006, //阻尼系数,系数越小滑动越灵敏  值越大滚动越慢
+			bounce: true //是否启用回弹
+		};
+		// 初始化区域滚动
+		mui('.mui-scroll-wrapper').scroll(options);
+	},
+	getCategoryLeft: function () {
+		$.ajax({
+			url: '/category/queryTopCategory',
+			success: function (backData) {
+				// console.log(backData);
+				var result = template('leftTem', backData);
+				$('.left ul').html(result);
+			}
+		})
+	},
+	getCategoryRight: function () {
+		$('.left ul').on('click', 'a', function (e) {
+			getData(1);
+			//console.log(e.target);
+
+			$(e.target.parentNode).addClass('active').siblings().removeClass('active');
+			// var current = $(e.target);
+			// var id = current.data('id');
+
+			var id = $(this).attr('data-id');
+			getData(id);
+			// 封装函数
+
+			function getData() {
+				$.ajax({
+					url: '/category/querySecondCategory',
+					data: {
+						id: id
+					},
+					success: function (backData) {
+						// console.log(backData);
+						var result = template('rightTem', backData);
+						if (result) {
+							$('.right .mui-row').html(result);
+						} else {
+							$('.right .mui-row').html('<h6>再下实在给不更多了</h6>');
+						}
+
+					}
+				});
+			}
+		})
+	}
+}
--- public/m/js/index.js
+++ public/m/js/index.js
@@ -1,9 +1,30 @@
-//获得slider插件对象
-var gallery = mui('.mui-slider');
-gallery.slider({
-  interval:2000//自动轮播周期，若为0则不自动播放，默认为0；
-});
-// 区域滚动
-mui('.mui-scroll-wrapper').scroll({
-	deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
-});
+$(function () {
+  var letao = new Letao();
+  letao.initSlide();
+  letao.initScroll();
+})
+var Letao = function () {
+ 
+}
+Letao.prototype = {
+  initSlide: function () {
+    //获得slider插件对象
+    var gallery = mui('.mui-slider');
+    gallery.slider({
+      interval: 2000 //自动轮播周期，若为0则不自动播放，默认为0；
+    });
+  },
+  initScroll: function () {
+    // 区域滚动
+    options = {
+      scrollY: true, //是否竖向滚动
+      scrollX: false, //是否横向滚动
+      startX: 0, //初始化时滚动至x
+      startY: 0, //初始化时滚动至y
+      indicators: true, //是否显示滚动条
+      deceleration: 0.0006, //阻尼系数,系数越小滑动越灵敏
+      bounce: true //是否启用回弹
+    }
+    mui('.mui-scroll-wrapper').scroll(options);
+  }
+}
